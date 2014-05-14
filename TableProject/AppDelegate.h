ace7781dc263ac3eda2cd6e1d4bd80f2c9b027ee
Daniel Rodosky <djrodosky@gmail.com>
2014-05-14 10:39:39 -0600

    initial commit
    initial commit

---
+++ TableProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableProject
+//
+//  Created by Daniel Rodosky on 5/14/14.
+//  Copyright (c) 2014 Dan Rodosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
